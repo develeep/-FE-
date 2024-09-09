884249e7261c780869969a360cba85ffe90fe12d
Lee <dltmdgns0508@gmail.com>
2024-09-09 15:43:31 +0900

    한입 FE챌린지 1day 과제

---
+++ firstDay/src/index.js
@@ -0,0 +1,12 @@
+// 1번
+let num1 = 10;
+let num2 = 25;
+console.log(num1 + num2);
+
+//2번
+const num = 1234;
+const str = 'hello';
+const undef = undefined;
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof undef);
